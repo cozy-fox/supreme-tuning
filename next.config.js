fc711ca09e745a8944bd27cd209eaa936e93f3af
Robert <ro.ll.484848@gmail.com>
2025-12-10 08:56:00 -0500

    update header frame-ancestors

--- next.config.js
+++ next.config.js
@@ -26,6 +26,20 @@ const nextConfig = {
     SITE_URL: process.env.SITE_URL || 'https://supremetuning.nl',
     SITE_NAME: 'Supreme Tuning',
   },
+  async headers() {
+    return [
+      {
+        // Apply headers to all routes
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://supremetuning.nl;"
+          }
+        ]
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig;

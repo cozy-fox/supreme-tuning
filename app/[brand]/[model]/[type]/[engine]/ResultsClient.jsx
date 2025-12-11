96935ac7bab2b0d671e31d66d95c4902d12a29dc
Robert <ro.ll.484848@gmail.com>
2025-12-11 17:34:28 -0500

    fix result page language

--- app/[brand]/[model]/[type]/[engine]/ResultsClient.jsx
+++ app/[brand]/[model]/[type]/[engine]/ResultsClient.jsx
@@ -364,7 +364,7 @@ function StageSection({ stage, stageIndex, vehicleInfo, isStage2, isAdmin, fetch
           <div className="power-stat">
             <div className="stat-header">
               <h4>{t('power')}</h4>
-              <span className="gain-badge">+{hpGain} PK</span>
+              <span className="gain-badge">+{hpGain} {t('enginePowerUnit')}</span>
             </div>
 
             {/* Stock Bar */}
@@ -372,7 +372,7 @@ function StageSection({ stage, stageIndex, vehicleInfo, isStage2, isAdmin, fetch
               <span className="bar-label stock-label">{t('stock')}</span>
               <div className="bar-track">
                 <div className="bar-fill bar-stock" style={{ width: `${(stockHp / maxPower) * 100}%` }}>
-                  <span className="bar-value">{stockHp} PK</span>
+                  <span className="bar-value">{stockHp} {t('enginePowerUnit')}</span>
                 </div>
               </div>
             </div>
@@ -382,7 +382,7 @@ function StageSection({ stage, stageIndex, vehicleInfo, isStage2, isAdmin, fetch
               <span className="bar-label tuned-label">{t('tuned')}</span>
               <div className="bar-track">
                 <div className="bar-fill bar-tuned" style={{ width: `${(tunedHp / maxPower) * 100}%` }}>
-                  <span className="bar-value">{tunedHp} PK</span>
+                  <span className="bar-value">{tunedHp} {t('enginePowerUnit')}</span>
                 </div>
               </div>
             </div>

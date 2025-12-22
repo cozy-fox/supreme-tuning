ff2dbe672413b09734eb12c28d36dd8168f32492
Robert <ro.ll.484848@gmail.com>
2025-12-22 16:39:43 -0500

    fix icon

--- components/HeroSection.jsx
+++ components/HeroSection.jsx
@@ -9,7 +9,8 @@ export default function HeroSection() {
   return (
     <div className="hero-section">
       <div className="hero-title-container">
-        <Zap size={40} color="var(--primary)" />
+        <img src="/assets/logo.png" alt="Supreme Tuning Logo" width={50} />
+        {/* <Zap size={40} color="var(--primary)" /> */}
         <h1>{t('chiptuningCalculator')}</h1>
       </div>
       <p className="hero-subtitle">
